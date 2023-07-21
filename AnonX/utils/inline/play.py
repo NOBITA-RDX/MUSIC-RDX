5578d020b722d90360d48740cbb1650a3b4b2536
NobitaRdX <140096318+NobitaRdX@users.noreply.github.com>
2023-07-21 22:20:50 +0530

    Update play.py

--- AnonX/utils/inline/play.py
+++ AnonX/utils/inline/play.py
@@ -16,23 +16,23 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     if 0 < anon <= 10:
         bar = "♪—————————"
     elif 10 < anon < 20:
-        bar = "—♥————————"
+        bar = "—♪————————"
     elif 20 <= anon < 30:
         bar = "——♪———————"
     elif 30 <= anon < 40:
-        bar = "———♥——————"
+        bar = "———♪——————"
     elif 40 <= anon < 50:
         bar = "————♪—————"
     elif 50 <= anon < 60:
-        bar = "—————♥————"
+        bar = "—————♪————"
     elif 60 <= anon < 70:
         bar = "——————♪———"
     elif 70 <= anon < 80:
-        bar = "———————♥——"
+        bar = "———————♪——"
     elif 80 <= anon < 95:
         bar = "————————♪—"
     else:
-        bar = "—————————♥"
+        bar = "—————————♪"
 
     buttons = [
         [
@@ -79,23 +79,23 @@ def telegram_markup_timer(_, chat_id, played, dur):
     if 0 < anon <= 10:
         bar = "♪—————————"
     elif 10 < anon < 20:
-        bar = "—♥————————"
+        bar = "—♪————————"
     elif 20 <= anon < 30:
         bar = "——♪———————"
     elif 30 <= anon < 40:
-        bar = "———♥——————"
+        bar = "———♪——————"
     elif 40 <= anon < 50:
         bar = "————♪—————"
     elif 50 <= anon < 60:
-        bar = "—————♥————"
+        bar = "—————♪————"
     elif 60 <= anon < 70:
         bar = "——————♪———"
     elif 70 <= anon < 80:
-        bar = "———————♥——"
+        bar = "———————♪——"
     elif 80 <= anon < 95:
         bar = "————————♪—"
     else:
-        bar = "—————————♥"
+        bar = "—————————♪"
 
     buttons = [
         [
