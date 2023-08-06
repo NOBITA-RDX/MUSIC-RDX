6261b33655ae9f63670ecb963b41a849da5f8cff
NobitaRdX <140096318+NobitaRdX@users.noreply.github.com>
2023-08-06 09:45:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/14b808ae60217e94b94d0.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/f5b292a7a67836da102f6.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = "https://graph.org/file/14b808ae60217e94b94d0.jpg"
+            START_IMG_URL = "https://graph.org/file/f5b292a7a67836da102f6.jpg"
