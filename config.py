e0d065390a70bb3a7502902608e4701f02e0c330
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-10-30 22:22:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,7 +76,7 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/d30e822eca739ff086d6a.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/4d81e33fcb94648c4d1b0.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
@@ -135,4 +135,4 @@ if PING_IMG_URL:
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/d30e822eca739ff086d6a.jpg")
+            START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/4d81e33fcb94648c4d1b0.jpg")
