b37569fdbedf2b65b398e0ac3eec465e36c02e64
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-11-06 12:29:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -76,11 +76,11 @@ clean = {}
 autoclean = []
 
 
-START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/4d81e33fcb94648c4d1b0.jpg")
+START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/b11ea2782ac0e7065ebbb.jpg")
 
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
-    "https://graph.org/file/314557031ff7ecde11f1d.jpg",
+    "https://telegra.ph/file/bb1cdf7971eda12cfee04.jpg",
 )
 
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
@@ -130,9 +130,9 @@ if UPSTREAM_REPO:
 if PING_IMG_URL:
     if PING_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", PING_IMG_URL):
-            PING_IMG_URL = "https://graph.org/file/314557031ff7ecde11f1d.jpg"
+            PING_IMG_URL = "https://telegra.ph/file/bb1cdf7971eda12cfee04.jpg"
 
 if START_IMG_URL:
     if START_IMG_URL != "assets/Ping.jpeg":
         if not re.match("(?:http|https)://", START_IMG_URL):
-            START_IMG_URL = getenv("START_IMG_URL", "https://graph.org/file/4d81e33fcb94648c4d1b0.jpg")
+            START_IMG_URL = getenv("START_IMG_URL", "https://telegra.ph/file/b11ea2782ac0e7065ebbb.jpg")
